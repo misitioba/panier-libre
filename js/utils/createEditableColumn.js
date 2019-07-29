aced8477e28a0357da00d3e17f33a8f38ec6c597
Javi <arancibiajav@gmail.com>
2019-07-29 20:14:59 +0200

    editable column prefetch cache added

--- js/utils/createEditableColumn.js
+++ js/utils/createEditableColumn.js
@@ -1,3 +1,6 @@
+let cacheResult = {}
+let cacheTimeout = {}
+
 export default function createEditableColumn({
     field,
     prefetch = null,
@@ -12,7 +15,55 @@ export default function createEditableColumn({
         async mounted(cmp) {
             let prefetchResult = null
             if (prefetch) {
-                prefetchResult = await api.funql(prefetch)
+                if (prefetch.cache) {
+                    var terminateCache = () => {
+                        if (cacheTimeout[prefetch.name]) {
+                            clearTimeout(cacheTimeout[prefetch.name])
+                            cacheTimeout[prefetch.name] = null
+                        }
+                        cacheTimeout[prefetch.name] = setTimeout(() => {
+                            delete cacheResult[prefetch.name]
+                            console.log('CACHE TERMINATED:', prefetch.name)
+                        }, prefetch.cache)
+                    }
+                    var waitCache = (success, timeout, start = Date.now()) => {
+                        return new Promise(async(resolve, reject) => {
+                            waitCacheLoop()
+
+                            async function waitCacheLoop() {
+                                if (Date.now() - start > 1000 * 5) {
+                                    await timeout()
+                                    resolve()
+                                } else {
+                                    if (cacheResult[prefetch.name] !== 'loading') {
+                                        success(cacheResult[prefetch.name])
+                                        resolve()
+                                    } else {
+                                        setTimeout(() => waitCacheLoop(), 50)
+                                    }
+                                }
+                            }
+                        })
+                    }
+                    if (cacheResult[prefetch.name] === 'loading') {
+                        await waitCache(
+                            cachedResult => {
+                                prefetchResult = cachedResult
+                                terminateCache()
+                            },
+                            async() => {
+                                prefetchResult = await api.funql(prefetch)
+                            }
+                        )
+                    } else {
+                        cacheResult[prefetch.name] = 'loading'
+                        prefetchResult = await api.funql(prefetch)
+                        cacheResult[prefetch.name] = prefetchResult
+                        terminateCache()
+                    }
+                } else {
+                    prefetchResult = await api.funql(prefetch)
+                }
             }
 
             created(cmp, v, prefetchResult || null)
