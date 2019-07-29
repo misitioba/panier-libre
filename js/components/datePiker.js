4d94163f459ef6278dfc4ed55e32de1498ed14a0
Javi <arancibiajav@gmail.com>
2019-07-29 20:15:41 +0200

    date picker can be reseted and vue model support

--- js/components/datePiker.js
+++ js/components/datePiker.js
@@ -1,12 +1,31 @@
+import {
+    default as stylesMixin,
+    template as styleMixinTmpl
+} from '../mixins/styles'
+
 Vue.component('date-picker', {
-    template: `
+    mixins: [stylesMixin],
+    props: ['value'],
+    template: styleMixinTmpl(`
     <div class="date_picker" ref="root">
-        <input type="text"  ref="picker"  />
-    </div>`,
+        <input type="text" placeholder="DD/MM/YYYY"  ref="picker"  />
+        <button class="btn" @click="clear">R</button>
+    </div>`),
     data() {
         return {
+            styles: `
+                .date_picker{
+                    display:flex;
+                }
+                input{
+                    margin-right:5px;
+                }
+                button{
+                    width: 50px !important;
+text-align: center !important;
+                }
+            `,
             selected: '',
-            items: [],
             transform: item => ({ value: item.value, text: item.text })
         }
     },
@@ -17,6 +36,13 @@ Vue.component('date-picker', {
             }
         })
     },
+    methods: {
+        clear() {
+            this.$refs.picker.value = ''
+            this.$emit('input', '')
+            this.$emit('keyup', '')
+        }
+    },
     mounted() {
         let self = this
         this.picker = new Pikaday({
@@ -24,9 +50,12 @@ Vue.component('date-picker', {
             format: 'DD/MM/YYYY',
             onSelect: function() {
                 self.$emit('input', this.getMoment()._d.getTime())
+                self.$emit('keyup', this.getMoment()._d.getTime())
             }
         })
-
+        if (this.value) {
+            this.picker.setMoment(moment(this.value))
+        }
         this.$emit('mounted', this)
     }
 })
