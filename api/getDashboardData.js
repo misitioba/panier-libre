538f7cb50c559f36246ae73004f6fba0fa201c12
Javi <arancibiajav@gmail.com>
2019-08-06 19:15:44 +0200

    observation added

--- api/getDashboardData.js
+++ api/getDashboardData.js
@@ -1,14 +1,14 @@
 module.exports = app => {
     return async function getDashboardData() {
         /* let query = `
-                                        SELECT bb.id, c.email, bb.quantity,b.id as basket_id, bb.date as booking_date, b.delivery_date, bb.is_canceled
-                                        FROM baskets as b
-                                        JOIN basket_bookings as bb on bb.basket_id = b.id
-                                        JOIN clients as c on c.id = bb.client_id
+                                                SELECT bb.id, c.email, bb.quantity,b.id as basket_id, bb.date as booking_date, b.delivery_date, bb.is_canceled
+                                                FROM baskets as b
+                                                JOIN basket_bookings as bb on bb.basket_id = b.id
+                                                JOIN clients as c on c.id = bb.client_id
 
-                                        ` */
+                                                ` */
         let query = `
-        SELECT orderItem.id, c.email, orderItem.quantity,b.id as basket_id, o.creation_date as booking_date, b.delivery_date, orderItem.is_canceled
+        SELECT orderItem.id, c.email, orderItem.quantity,b.id as basket_id, o.creation_date as booking_date, b.delivery_date, orderItem.is_canceled, o.observation as observation
         FROM baskets as b
 JOIN order_items as orderItem on orderItem.basket_id = b.id
 JOIN orders as o on o.id = orderItem.order_id
