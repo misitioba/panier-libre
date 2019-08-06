c045500b7b75e7e31a5033fde29c32bdf9be5fb7
Javi <arancibiajav@gmail.com>
2019-08-06 19:16:43 +0200

    bookings table removed

--- api/getBaskets.js
+++ api/getBaskets.js
@@ -1,15 +1,5 @@
 module.exports = app => {
     return async function getBaskets() {
-        /* let query = `SELECT
-                                b.*,
-                                count(
-                                    bb.client_id) as bookings,
-                                b.quantity-count(
-                                    bb.client_id) as available
-                                FROM baskets as b
-                                    LEFT JOIN basket_bookings as bb on bb.basket_id = b.id and bb.is_canceled = 0
-                                GROUP BY b.id
-                                ` */
         let query = `
         SELECT 
 b.*,
