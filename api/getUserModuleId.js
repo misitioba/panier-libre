ea1820179356e2b27f0d00cc57c3013e4dc1f846
Javi <arancibiajav@gmail.com>
2019-08-17 11:40:52 +0200

    solved guest session error

--- api/getUserModuleId.js
+++ api/getUserModuleId.js
@@ -1,6 +1,9 @@
 module.exports = app => {
     return async function getUserModuleId(id) {
         // Return the id of the user module (basket_hot) tied to the current user
+        if(!this.user){
+            return 0
+        }
         let match = this.user.modules.find(um => um.module_id == this.moduleId)
         return match && match.id || 0
     }
