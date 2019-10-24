1753833e0ad87e6c2cb3b5748f2ebfaaebd35c31
Javi <arancibiajav@gmail.com>
2019-10-24 12:49:59 +0200

    add home view

--- js/app.js
+++ js/app.js
@@ -16,8 +16,8 @@ import './components/inputCmp'
 import './components/appFooter'
 
 import Home from './containers/home'
+import Baskets from './containers/baskets'
 import Dashboard from './containers/dashboard'
-import Baskets from './containers/bookBasket'
 import Clients from './containers/clientsList'
 import Integration from './containers/integrationPage'
 import BasketModels from './containers/basketModels'
@@ -25,15 +25,18 @@ import Programation from './containers/programation'
 import Orders from './containers/orders'
 import OrdersDetails from './containers/orderDetails'
 import Empty from './containers/empty'
+import Login from './containers/login'
 ImportCDNJS('/analytics.js')
 
 window.ERROR = {
     API: 'Erreur de serveur ou de connexion'
 }
 const routes = [
-    { path: '/', component: Dashboard, name: 'dashboard' },
+    { path: '/', component: Home, name: 'home' },
+    { path: '/dashboard', component: Dashboard, name: 'dashboard' },
+    { path: '/login', component: Login, name: 'login' },
     { path: '/logout', component: Empty, name: 'logout' },
-    { path: '/baskets', component: Home, name: 'baskets' },
+    { path: '/baskets', component: Baskets, name: 'baskets' },
     { path: '/orders', component: Orders, name: 'orders' },
     { path: '/orders/:id', component: OrdersDetails, name: 'order-details' },
     { path: '/clients-list', component: Clients, name: 'clients' },
@@ -49,10 +52,10 @@ const router = new VueRouter({
 
 // window.router = router
 window.onLogout = () => {
-    router.push({name:'logout'})
+    router.push({ name: 'home' })
 }
 window.onLogin = () => {
-    router.push({name:'dashboard'})
+    router.push({ name: 'dashboard' })
 }
 
 new Vue({
@@ -62,9 +65,5 @@ new Vue({
     data() {
         return {}
     },
-    created() {
-        console.log('APP created')
-    }
+    created() {}
 })
-
-console.log('APP')
--- js/containers/home.js
+++ js/containers/home.js
@@ -1,62 +1,88 @@
+import {
+    default as stylesMixin,
+    template as styleMixinTmpl
+} from '../mixins/styles'
+import authMixin from '../mixins/auth'
 export default {
-    name: 'HomeComponent',
-    template: `
-    <div>
-    <!--
-        <nav-component :enabled="isNavEnabled"></nav-component>
-        -->
-        <basket-list v-show="isNavEnabled" @addBasket="addBasket" @editBasket="editBasket" ref="basketList"></basket-list>
-        
-        <modal-window ref="modal" v-show="!!modalCmp" v-model="modalCmp" :params="modalCmpParams" @close="closeModal"></modal-window>
+    mixins: [stylesMixin, authMixin],
+    name: 'Home',
+    template: styleMixinTmpl(`
+    <div ref="root">
+    <h2>Bienvenue, vous êtes dans l'instance {{currentInstanceTitle}}</h2>
+    <h4>{{currentInstance.title}}</h4>
+    <a class="InstanceLink" :href="currentInstance.url" target="_blank" v-html="currentInstance.url"></a>
+    <div class="btn_group" style="margin-top:10px;">
+        <router-link class="btn" :to="{name:'dashboard'}">Aller au tableau de bord</router-link>
     </div>
-    `,
+    <h3>Autres serveurs</h3>
+    <ul>
+        <li v-for="item in otherInstances">
+            <h4>{{item.title}}</h4>
+            <a class="InstanceLink" :href="item.url" target="_blank" v-html="item.url"></a>
+        </li>
+    </ul>
+    <h3>Créez votre propre instance</h3>
+    <p>Suivez les instructions pour intégrer une nouvelle instance de serveur dans l'écosystème.</p>
+    <a href="https://gitlab.com/misitioba/panier-libre/tree/master" target="_blank">
+    https://gitlab.com/misitioba/panier-libre/tree/master
+    </a>
+    </div>
+    `),
     data() {
         return {
-            user: {
-                id: null
-            },
-            modalCmp: '',
-            modalCmpParams: {
-                foo: 1
+            styles: `
+            .InstanceLink{
+                font-size:10px;
             }
+            @media only screen and (max-width: 639px) {
+                
+            }
+            @media only screen and (max-width: 1047px) {
+                
+            }
+            @media only screen and (min-width: 1048px) {
+                
+            }
+        `
         }
     },
-    methods: {
-        closeModal() {
-            this.modalCmp = ''
-            this.$refs.basketList.$emit('refresh')
-        },
-        addBasket() {
-            this.modalCmpParams = {
-                mode: 'create'
-            }
-            this.modalCmp = 'basket-details'
-        },
-        editBasket(id) {
-            this.modalCmpParams = {
-                id,
-                title: 'Edition',
-                mode: 'edit'
-            }
-            this.modalCmp = 'basket-details'
-        }
-    },
+    methods: {},
     computed: {
-        isNavEnabled() {
-            return (this.user && !!this.user.id) || false
-        }
-    },
-    created() {
-        if (window.user && window.user.id != this.user.id) {
-            this.user.id = window.user.id
-        }
-        this.interval = setInterval(() => {
-            if (window.user && window.user.id != this.user.id) {
-                this.user.id = window.user.id
+        currentInstanceTitle() {
+            let match = this.instances.find(
+                item => item.url.indexOf(window.location.origin) !== -1
+            )
+            if (match) {
+                return match.title
+            } else {
+                return `${window.location.origin} (Non-officiel)`
             }
-        }, 500)
+        },
+        currentInstance() {
+            let match = this.instances.find(
+                item => item.url.indexOf(window.location.origin) !== -1
+            )
+            if (match) {
+                return match
+            } else {
+                return {}
+            }
+        },
+        otherInstances() {
+            return this.instances.filter(item => item.url != this.currentInstance.url)
+        },
+        instances() {
+            if (window.location.origin.indexOf('localhost') !== -1) {
+                if (!window.instances.find(i => i.url == window.location.origin)) {
+                    window.instances.unshift({
+                        title: 'Localhost',
+                        url: window.location.origin
+                    })
+                }
+            }
+            return window.instances
+        }
     },
-    destroyed() {
-        clearInterval(this.interval)
-    }
+    created() {},
+    destroyed() {}
 }
